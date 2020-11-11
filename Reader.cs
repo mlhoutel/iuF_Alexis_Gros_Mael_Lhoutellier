95a00a7dc859869bc95d1051757a27d3a723ef18
LHOUTELLIER Maël <55429341+mlhoutel@users.noreply.github.com>
2020-11-11 17:54:53 +0100

    Init

---
+++ Reader.cs
@@ -0,0 +1,53 @@
+﻿using Intel.RealSense;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace iuF {
+    public static class Reader {
+        private static Config ConfigFile(this Config config, string filename) {
+            config.EnableDeviceFromFile(filename, repeat: false); 
+            return config; 
+        }
+        public static Pipeline FromFile(string filename) {
+            if (!File.Exists(filename)) { Console.WriteLine("Error: file {0} does not exists.", filename); return null; }
+
+            Pipeline pipeline = new Pipeline();
+            Config config = ConfigFile(new Config(), filename);
+            PipelineProfile profile = pipeline.Start(config);
+
+            Device device = profile.Device;
+            PlaybackDevice playback = PlaybackDevice.FromDevice(device);
+
+            DateTime begins = DateTime.Now;
+
+            Console.WriteLine("Reading file {0}\nDuration time: {1}\nCamera name: {2}\nSerial number: {3}\nFirmware version: {4}\n", filename, TimeSpan.FromMilliseconds(playback.Duration * 1e-6), device.Info[CameraInfo.Name], device.Info[CameraInfo.SerialNumber], device.Info[CameraInfo.FirmwareVersion]);
+
+            playback.Realtime = false;
+
+            return pipeline;
+        }
+
+        private static Config ConfigDevice(this Config config) { 
+            config.EnableStream(Intel.RealSense.Stream.Depth);
+            config.EnableStream(Intel.RealSense.Stream.Color);
+            return config; 
+        }
+        public static Pipeline FromDevice() {
+            Context context = new Context();
+            DeviceList devices = context.QueryDevices();
+
+            if (devices.Count == 0) { Console.WriteLine("Error: there is no connected devices."); return null; }
+            Device device = devices[0];
+            Console.WriteLine("Using device 0\nCamera name: {0}\nSerial number: {1}\nFirmware version: {2}\n", device.Info[CameraInfo.Name], device.Info[CameraInfo.SerialNumber], device.Info[CameraInfo.FirmwareVersion]);
+
+            Pipeline pipeline = new Pipeline();
+            Config config = ConfigDevice(new Config());
+            PipelineProfile profile = pipeline.Start(config);
+
+            return pipeline;
+        }
+
+    }
+}
---
+++ Streamer.cs
@@ -0,0 +1,56 @@
+﻿using Intel.RealSense;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iuF {
+    public static class Streamer {
+
+        const UInt16 CAMERA_WIDTH = 640;
+        const UInt16 CAMERA_HEIGHT = 480;
+        const UInt16 CAMERA_FRAMERATE = 30;
+
+        private static UInt16[] DepthArray(VideoFrame depthFrame) {
+            UInt16[] depthArray = new UInt16[CAMERA_WIDTH * CAMERA_HEIGHT];
+            depthFrame.CopyTo(depthArray);
+            return depthArray; 
+        }
+        private static byte[] ColorArray(VideoFrame colorFrame) {
+            byte[] colorArray = new byte[CAMERA_WIDTH * CAMERA_HEIGHT * 3];
+            colorFrame.CopyTo(colorArray);
+            return colorArray;
+        }
+        public static void Stream(Pipeline pipeline) {
+            if (pipeline == null) { return; }
+
+            int frame = 0;
+            FrameSet frames;
+            while (pipeline.TryWaitForFrames(out frames)) {
+                Console.WriteLine("Frame number {0}", frame++);
+                Frame(frames);
+                System.Threading.Thread.Sleep(30);
+            }
+        }
+
+        private static void Frame(FrameSet frames) {
+            Align align = new Align(Intel.RealSense.Stream.Color).DisposeWith(frames);
+            Frame aligned = align.Process(frames).DisposeWith(frames);
+            FrameSet frameset = aligned.As<FrameSet>().DisposeWith(frames);
+
+            VideoFrame colorFrame = frameset.ColorFrame.DisposeWith(frameset);
+            VideoFrame depthFrame = frameset.DepthFrame.DisposeWith(frameset);
+
+            byte[] colorArray = ColorArray(colorFrame);
+            UInt16[] depthArray = DepthArray(depthFrame);
+            AsciiDepth(depthArray);
+        }
+
+        private static void AsciiDepth(UInt16[] depthArray) {
+            for (int y = 0; y < CAMERA_HEIGHT; y++) {
+                for (int x = 0; x < CAMERA_WIDTH; x++) {
+                }
+            }
+            //System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
